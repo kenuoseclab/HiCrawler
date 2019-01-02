8f30410efef320bcd3af6015ef0d3ba75a5be1dc
sl-say <hiexam@126.com>
2019-01-02 13:40:07 +0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/HiCrawler/precache-manifest.d8af80ee5d99095a2fe2084ea286ee77.js"
+  "./precache-manifest.fca42ddb9d8fe6908b8514f1bb7a3b8e.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/HiCrawler/index.html", {
+workbox.routing.registerNavigationRoute("./index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
