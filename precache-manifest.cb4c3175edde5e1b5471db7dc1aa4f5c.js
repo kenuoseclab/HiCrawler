2683761fef483761e3093fa43f2853b2c49fbaa1
sl_yang <sl_yang@dreamarts.co.jp>
2019-07-07 00:49:59 +0800

    更换logo

--- precache-manifest.cb4c3175edde5e1b5471db7dc1aa4f5c.js
+++ precache-manifest.cb4c3175edde5e1b5471db7dc1aa4f5c.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "f05b50aaa1632229df997c8be961e123",
+    "revision": "08110157408d0a2cf00e1895fb01f29b",
     "url": "/index.html"
   },
   {
@@ -8,8 +8,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.d071cd62.chunk.css"
   },
   {
-    "revision": "4528a58c54b759601a9b",
-    "url": "/static/css/main.a8b29a9d.chunk.css"
+    "revision": "7834e9578adafff1e797",
+    "url": "/static/css/main.40e58e7f.chunk.css"
   },
   {
     "revision": "aae4c15a4f68ad826e9c",
@@ -20,15 +20,11 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/3.cf6b9d35.chunk.js"
   },
   {
-    "revision": "4528a58c54b759601a9b",
-    "url": "/static/js/main.ed38aa00.chunk.js"
+    "revision": "7834e9578adafff1e797",
+    "url": "/static/js/main.18ad1c22.chunk.js"
   },
   {
     "revision": "95991d8e8f2711bc5bdb",
     "url": "/static/js/runtime~main.08e00f94.js"
-  },
-  {
-    "revision": "e5c39b444a39cdec7f8be16cc46c66b4",
-    "url": "/static/media/logo.e5c39b44.png"
   }
 ]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.54f529513b6caa7a6ee1fe42bfbd2117.js"
+  "/precache-manifest.cb4c3175edde5e1b5471db7dc1aa4f5c.js"
 );
 
 self.addEventListener('message', (event) => {
